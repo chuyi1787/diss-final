a0ab65e9d851b257a3fce3ab1ce525e0f9f7f38f
chenchuyi <chenchuyi@xuetangx.com>
2020-08-08 22:48:10 +0100

    new

---
+++ trainschedule-full.sh
@@ -0,0 +1,62 @@
+sh train-gpu-full.sh models-bpe 100-bpe-20-context Englsih
+sh train-gpu-full.sh models-bpe 100-bpe-20-context Turkish
+sh train-gpu-full.sh models-bpe 100-bpe-20-context Spanish
+sh train-gpu-full.sh models-bpe 100-bpe-20-context Arabic
+sh train-gpu-full.sh models-bpe 100-bpe-20-context Indonesian
+
+sh train-gpu-lite.sh models-bpe 500-bpe-20-context-lite Englsih
+sh train-gpu-lite.sh models-bpe 500-bpe-20-context-lite Turkish
+sh train-gpu-lite.sh models-bpe 500-bpe-20-context-lite Spanish
+sh train-gpu-lite.sh models-bpe 500-bpe-20-context-lite Arabic
+sh train-gpu-lite.sh models-bpe 500-bpe-20-context-lite Indonesian
+
+sh train-gpu-full.sh models-bpe 1000-bpe-20-context Englsih
+sh train-gpu-full.sh models-bpe 1000-bpe-20-context Turkish
+sh train-gpu-full.sh models-bpe 1000-bpe-20-context Spanish
+sh train-gpu-full.sh models-bpe 1000-bpe-20-context Arabic
+sh train-gpu-full.sh models-bpe 1000-bpe-20-context Indonesian
+
+sh train-gpu-full.sh models-bpe 3000-bpe-20-context Englsih
+sh train-gpu-full.sh models-bpe 3000-bpe-20-context Turkish
+sh train-gpu-full.sh models-bpe 3000-bpe-20-context Spanish
+sh train-gpu-full.sh models-bpe 3000-bpe-20-context Arabic
+sh train-gpu-full.sh models-bpe 3000-bpe-20-context Indonesian
+
+sh train-gpu-full.sh models-bpe 5000-bpe-20-context Englsih
+sh train-gpu-full.sh models-bpe 5000-bpe-20-context Turkish
+sh train-gpu-full.sh models-bpe 5000-bpe-20-context Spanish
+sh train-gpu-full.sh models-bpe 5000-bpe-20-context Arabic
+sh train-gpu-full.sh models-bpe 5000-bpe-20-context Indonesian
+
+sh train-gpu-full.sh models-bpeall 100-bpeall-20-context Englsih
+sh train-gpu-full.sh models-bpeall 100-bpeall-20-context Turkish
+sh train-gpu-full.sh models-bpeall 100-bpeall-20-context Spanish
+sh train-gpu-full.sh models-bpeall 100-bpeall-20-context Arabic
+sh train-gpu-full.sh models-bpeall 100-bpeall-20-context Indonesian
+
+sh train-gpu-lite.sh models-bpeall 500-bpeall-20-context-lite Englsih
+sh train-gpu-lite.sh models-bpeall 500-bpeall-20-context-lite Turkish
+sh train-gpu-lite.sh models-bpeall 500-bpeall-20-context-lite Spanish
+sh train-gpu-lite.sh models-bpeall 500-bpeall-20-context-lite Arabic
+sh train-gpu-lite.sh models-bpeall 500-bpeall-20-context-lite Indonesian
+
+sh train-gpu-full.sh models-bpeall 1000-bpeall-20-context Englsih
+sh train-gpu-full.sh models-bpeall 1000-bpeall-20-context Turkish
+sh train-gpu-full.sh models-bpeall 1000-bpeall-20-context Spanish
+sh train-gpu-full.sh models-bpeall 1000-bpeall-20-context Arabic
+sh train-gpu-full.sh models-bpeall 1000-bpeall-20-context Indonesian
+
+sh train-gpu-full.sh models-bpeall 3000-bpeall-20-context Englsih
+sh train-gpu-full.sh models-bpeall 3000-bpeall-20-context Turkish
+sh train-gpu-full.sh models-bpeall 3000-bpeall-20-context Spanish
+sh train-gpu-full.sh models-bpeall 3000-bpeall-20-context Arabic
+sh train-gpu-full.sh models-bpeall 3000-bpeall-20-context Indonesian
+
+sh train-gpu-full.sh models-bpeall 5000-bpeall-20-context Englsih
+sh train-gpu-full.sh models-bpeall 5000-bpeall-20-context Turkish
+sh train-gpu-full.sh models-bpeall 5000-bpeall-20-context Spanish
+sh train-gpu-full.sh models-bpeall 5000-bpeall-20-context Arabic
+sh train-gpu-full.sh models-bpeall 5000-bpeall-20-context Indonesian
+
+
+
